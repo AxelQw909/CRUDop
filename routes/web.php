99542c00cfbe0471e214fcc414be282696949119
AxelQw909 <dart58215@gmail.com>
2025-11-08 10:11:41 +0500

    регистрация

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/register', function () {
+    return view('register');
+});
+
+use Illuminate\Http\Request;
+
+Route::post('/register', function (Request $request) {
+    $validated = $request->validate([
+        'name' => 'required|min:2',
+        'email' => 'required|email',
+        'password' => 'required|min:6|confirmed'
+    ]);
+    return "Регистрация успешна!";
+})->name('register.submit');
